f41567c4b3bef3d1e90718dcb72456cc0ce8f162
Pawel <pawelkordel@gmail.com>
2021-12-09 05:58:23 +0100

    Zadanie 5

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         /* ćwiczenie 5 Metody */
+
 
         /* zad.1
         a) stworzyć tablicę typu int[20],
@@ -13,13 +15,20 @@ public class Main {
         d) wstawić wywołanie metody do metody main
         */
 
+        int [] tab = metoda1(20);
+        metoda2(tab);
+        System.out.println();
+
         /* zad.2
         a) stworzyc tablicę typu String[6],
         b) napisać metodę która uzupełni tablicę wartościami podawanymi z klawiatury
             (użyć do tego pętli i klasy Scanner),
         c) napisać metodę która wyświetli elementy tablicy od ostatniego elementu do elementu o indeksie 0,
         */
-
+        System.out.println("Podaj 6 wyrazow przedzielonych spacja: ");
+        String [] tab2 = metoda3(6);
+        metoda4(tab2);
+        System.out.println();
         /* zad.3
         a) napisać metodę która przyjmuje 1 parametr typu String, metoda nic nie zwraca, tylko
             wyświetla w konsoli string który ma być waszym imieniem,
@@ -29,6 +38,11 @@ public class Main {
             nazwisko i wiek (int), a następnie wszystko wyświetli,
         */
 
+        metoda5("Pawel");
+        metoda5("Pawel","Kordel");
+        metoda5("Pawel", "Kordel", 40);
+        System.out.println();
+
         /* zad.4
         a) napisać metodę która przyjmuje 2 parametry typu int i zwraca ich sumę, którą przypiszemy do zmiennej,
             coś takiego:                                    zmiennaA = metoda(1, 2); <-- wywołanie metody
@@ -37,7 +51,59 @@ public class Main {
         c) wyświetlić sumę wywołań obu metod ---> print("wynik: " + (metoda(8, 9) + metoda(2, 3, 4));
         */
 
+        int zmienna1 = metoda6(3,6);
+        int zmienna2 = metoda6(4,8,9);
+        System.out.println("Suma obu metod wynosi: " + (zmienna1 + zmienna2));
+    }
 
+    public static int [] metoda1(int rozmiar){
+
+        int [] tab0 = new int [rozmiar];
+        for (int i=0; i<rozmiar; i++){
+            tab0[i]=40-i;
+        }
+        return tab0;
+    }
+
+    public static void metoda2 (int [] tab){
+        for (int i : tab){
+            System.out.print(i + ", ");
+        }
+    }
+
+    public static String[] metoda3(int wielkosc) {
+        Scanner wpisz = new Scanner(System.in);
+        String[] tabs = new String[wielkosc];
+        for (int i = 0; i <= wielkosc-1; i++) {
+            tabs[i] = wpisz.next();
+        }
+        return tabs;
+    }
+
+    public static void metoda4(String[]tab){
+        for (int i = 5; i>=0; i--){
+            System.out.print(tab[i] + ", ");
+        }
+    }
+
+    public static void metoda5(String imie) {
+        System.out.println(imie);
+    }
+
+    public static void metoda5(String imie, String nazwisko){
+        System.out.println(imie + " " + nazwisko);
+    }
+
+    public static void metoda5(String imie, String nazwisko, int wiek){
+        System.out.println(imie + " " + nazwisko + " : " + wiek);
+    }
+
+    public static int metoda6(int a, int b){
+        return a+b;
+    }
+
+    public static int metoda6(int a, int b, int c){
+        return a+b+c;
     }
 
 }
