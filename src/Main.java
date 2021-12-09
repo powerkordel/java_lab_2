5806c41d27510878cafc805fca2f6b724012560c
Pawel <pawelkordel@gmail.com>
2021-12-09 02:40:36 +0100

    Zadanie 4

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,7 +11,39 @@ public class Main {
         b) w drugiej pętli continue
         c) w trzeciej pętli return
         */
+        //a)
+        int a = 20;
+        int i;
+
+        System.out.println("Zatrzymaj na 3/4 iteracji:");
+
+        for (i = a; i > 0; i--) {
+            if (i == (a *0.75))
+                break;
+            System.out.print(i + " ");
+        }
+        //b)
+        System.out.println();
+        System.out.println("Liczby parzyste to: ");
+        for(i=a;i>0;i--) {
+        if (i % 2 != 0)
+            continue;
+        System.out.print(i + ", ");
+        }
+        //c)
+        System.out.println();
+        System.out.println("Zatrzymaj w polowie iteracji: ");
+        for (i = a; i > 0 ; i--) {
+            if (i == a/2 )
+                return;
+            System.out.print(i + " ");
+        }
 
 
-    }
+
+
+
+}
+
+
 }
